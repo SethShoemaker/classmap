86b7e1f25bad73758bad044e20fec67820393b67
Seth Shoemaker <contact@sethshoemaker.com>
2026-02-26 18:54:12 -0500

    add prod docker stuff

--- public-webserver/helpers/query-params.ts
+++ public-webserver/helpers/query-params.ts
@@ -1,15 +1,17 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export function nullStringMiddleware(request: Request, response: Response, next: NextFunction) {
     Object.defineProperty(request, 'query', { ...Object.getOwnPropertyDescriptor(request, 'query'), value: request.query, writable: true });
     for (const [key, value] of Object.entries(request.query)) {
         if (value == 'null') {
+            // @ts-ignore
             request.query[key] = null;
             continue;
         }
         if (Array.isArray(value)) {
             const nullPos = value.indexOf('null');
             if (nullPos != -1) {
+                // @ts-ignore
                 value[nullPos] = null;
             }
             continue;
@@ -19,10 +21,10 @@ export function nullStringMiddleware(request: Request, response: Response, next:
     next();
 }
 
-export function queryParamValueArray(queryParam: string, request: Request): Array<string> {
+export function queryParamValueArray(queryParam: string, request: Request): Array<string | null> {
     const query = request.query[queryParam];
 
-    if (!query) {
+    if (query === undefined) {
         return [];
     }
 
@@ -31,5 +33,9 @@ export function queryParamValueArray(queryParam: string, request: Request): Arra
         return query;
     }
 
-    return query.toString().split(',');
+    if (query == null) {
+        return [null];
+    }
+
+    return query.toString().split(',').map(s => s == '' ? null : s);
 }
