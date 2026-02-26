9f4743dce12bcd478942a0c1cf3c6209bf11f8b2
Seth Shoemaker <contact@sethshoemaker.com>
2026-02-26 17:32:12 -0500

    fix typescript errors

--- internal-api/helpers/query-params.ts
+++ internal-api/helpers/query-params.ts
@@ -1,15 +1,17 @@
-import { Request } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export function nullStringMiddleware(request: Request, response: Response, next: NextFunction) {
     Object.defineProperty(request, 'query', { ...Object.getOwnPropertyDescriptor(request, 'query'), value: request.query, writable: true });
     for (const [key, value] of Object.entries(request.query)) {
         if (value == 'null') {
+            // @ts-ignore
             request.query[key] = null;
             continue;
         }
         if (Array.isArray(value)) {
             const nullPos = value.indexOf('null');
             if (nullPos != -1) {
+                // @ts-ignore
                 value[nullPos] = null;
             }
             continue;
